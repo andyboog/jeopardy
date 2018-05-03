187d2aaac97ad7602e0d1e0e9c09e6e8f5f21217
andyboog <38238789+andyboog@users.noreply.github.com>
2018-05-03 16:42:20 -0700

    Update trivia.h

--- trivia.h
+++ trivia.h
@@ -1,1 +1,21 @@
-//this will be the code for the trivia questions
+#include<iostream>
+#include<queue>
+#include<vector>
+
+//so once I load more of the questions, we can just have the player ask for a category and the number of points
+//the number of points will determine which question is picked
+//let me know if you would like to do something different
+
+bool gaming(int points){
+    cout << "Backup Agents in Elite Beat Agents." << endl;
+    cout << "A. Who are Agents Morris and Derek?" << endl;
+    cout << "B. Who are Agents K and J?" << endl;
+    cout << "C. Who are Agents Mulder and Skully?" << endl;
+    cout << "type answer here: ";
+    char answer;
+    cin >> answer;
+    if(toupper(answer) == 'A'){
+        return true;
+    }
+    else return false;
+}
