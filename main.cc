f88eb1364037f704153fbe4e91ad81dc4da1ec3b
andyboog <38238789+andyboog@users.noreply.github.com>
2018-05-03 17:26:08 -0700

    Update main.cc

--- main.cc
+++ main.cc
@@ -8,10 +8,83 @@
 using namespace std;
 
 int main() {
-
+	queue<int>scores;
+	score.push(0);
 	cout << "Welcome to..." << endl;
 	jeopardyArt();
-	cout << "Here's your first question for 100 points" << endl;
+	cout << "To start this game, you will need to choose from which category the question will come." << endl;
+	cout << "The categories offered are: GAMING, NORSE MYTHOLOGY (not the Marvel kind), AMERICAN HISTORY," << endl;
+	cout << "JEOPARDY, DISNEY, and BOWLING TERMS." << endl;
+	cout << "Note: please type the category names as they are typed above." << endl;
+	string category;
+	cout << "Category: ";
+	cin >> category;
+	cout << "Awesome! Next you will need to choose how many points you would like the question to be worth." << endl;
+	cout << "The points range from 100 - 600 by increments of 100." << endl;
+	cout << "Note: You can only choose each point value once throughout the game." << endl;
+	int points;
+	cin >> points;
+	if(category == "GAMING"){
+		if(gaming(points)){
+			cout << "That is correct!" << endl;
+			scores.push(correct(scores, points));
+		}
+		else {
+			cout << "Sorry! That is incorrect!" << endl;
+			scores.push(incorrect(scores, points));
+		}
+		cout << "Current Point Value: " << scores.back() << endl;
+
+	}
+	else if (category == "NORSE MYTHOLOGY"){
+		if(NorseMythology(points)){
+			cout << "That is correct!" << endl;
+			scores.push(correct(scores, points));
+		}
+		else {
+			cout << "Sorry! That is incorrect!" << endl;
+			scores.push(incorrect(scores, points));
+		}
+		cout << "Current Point Value: " << scores.back() << endl;
+
+	}
+	else if (category == "AMERICAN HISTORY"){
+		if(AmericanHistory(points)){
+			cout << "That is correct!" << endl;
+			scores.push(correct(scores, points));
+		}
+		else {
+			cout << "Sorry! That is incorrect!" << endl;
+			scores.push(incorrect(scores, points));
+		}
+		cout << "Current Point Value: " << scores.back() << endl;
+
+	}
+	else if (category == "DISNEY"){
+		if(disney(points)){
+			cout << "That is correct!" << endl;
+			scores.push(correct(scores, points));
+		}
+		else {
+			cout << "Sorry! That is incorrect!" << endl;
+			scores.push(incorrect(scores, points));
+		}
+		cout << "Current Point Value: " << scores.back() << endl;
+
+	}
+	else if (category == "BOWLING TERMS"){
+		if(BowlingTerms(points)){
+			cout << "That is correct!" << endl;
+			scores.push(correct(scores, points));
+		}
+		else {
+			cout << "Sorry! That is incorrect!" << endl;
+			scores.push(incorrect(scores, points));
+		}
+		cout << "Current Point Value: " << scores.back() << endl;
+
+	}
+	/*cout << "Here's your first question for 100 points" << endl;
 	cout << "What's the first letterin the alphabet?" << endl;
 	cout << "a. A" << endl;
 	cout << "b. B" << endl;
@@ -39,5 +112,6 @@ int main() {
 		cout << "That was incorrect!" << endl;
 	}
 	cout << "Congratulations you finished with " << scores.back() << " points!" << endl;
+	*/
 }
 
