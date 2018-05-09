f923e168a302f8ac9ea1c37a8abd1ff30a000acc
andyboog <38238789+andyboog@users.noreply.github.com>
2018-05-09 20:41:44 +0000

    Create linkedlist.h

---
+++ linkedlist.h
@@ -0,0 +1,11 @@
+//so I found out that we have to make our own data structures, so that's what I'm gonna do here
+//please change anything that you would like to
+#include<iostream>
+struct node{
+  int data;
+  struct node *next;
+ } *head;
+ 
+void addtolist(int x){
+  
+ 
