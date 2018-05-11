e9f97ce007a8d91bc04974a1451b7e17440e5cc7
andyboog <38238789+andyboog@users.noreply.github.com>
2018-05-10 20:19:08 -0700

    Update scores.h

--- scores.h
+++ scores.h
@@ -1,18 +1,18 @@
 #include<iostream>
 #include<cmath>
-#include<queue>
+#include "queue.h"
 using namespace std;
 
 // "int b" should be the amount of points the player receives
 // for answering the question correctly
-int correct(queue<int>a, int b) {
+int correct(queue a, int b) {
     int new_points = a.back() + b;
     return new_points;
 }
 
 // "int c" should be the amount of points the player loses
 // for answering the question incorrectly
-int incorrect(queue<int>c, int d) {
+int incorrect(queue c, int d) {
     int new_points = c.back() - d;
     return new_points;
 }
